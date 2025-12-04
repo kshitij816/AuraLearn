5778d6c5e758053b41e3891ea9d3755cc92c8d41
Kshitij Trivedi <kshitijtrivedi816@gmail.com>
2025-12-05 00:04:17 +0530

    Fix gitignore and clean repo

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+
+const Page = () => {
+  return (
+    <main>
+        <h1 className="text-2xl">Popular Companions</h1>
+
+        <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the Brainy Explorer"
+                topic="Neural Network of the Brain"
+                subject="science"
+                duration={45}
+                color="#ffda6e"
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Derivatives & Integrals"
+                subject="Maths"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id="789"
+                name="Verba the Vocabulary Builder"
+                topic="English Literature"
+                subject="Language"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title ="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA
+
+            />
+        </section>
+    </main>
+  )
+}
+
+export default Page
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="Logo" width={50} height={50}
+                    />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
